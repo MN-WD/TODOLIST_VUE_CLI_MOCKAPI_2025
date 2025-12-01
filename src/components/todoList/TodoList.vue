a71a5e6c53f700b53e3f8e8ef8337ec814148453
MN <bmdaetl@gmail.com>
2025-12-01 15:26:11 +0100

    Store stores/todos.js

--- src/components/todoList/TodoList.vue
+++ src/components/todoList/TodoList.vue
@@ -1,6 +1,6 @@
 <script setup>
-  import { reactive, onMounted, computed } from 'vue';
-  import DB from '@/services/DB';
+  import { onMounted } from 'vue';
+  import { todosStore } from "@/stores/todos";
   import TodoListAddForm from './TodoListAddForm.vue';
   import TodoListFooter from './TodoListFooter.vue';
   import Todo from './Todo.vue';
@@ -9,36 +9,12 @@
     apiURL: { type: String, required: true },
   });
 
-  const todos = reactive([]);
-
-  const notCompletedCount = computed(
-    () => todos.filter(todo => !todo.completed).length
-  );
-
   // onMounted quand des objets sont créés dans le DOM, on prend le tableau todos et on y met le résultat de la transaction ajax pour chercher les todos dans l'API
   // On intervient quand les éléments (todos) sont créés dans le DOM, on y met les infos de la DB
+  // Quand on a déclaré todos c'est une constante, on le splice en tableau, todos.length = là où on commence à ajouter/enlever des éléments (= 0), 0 = on remplace 0 éléments, ... = syntaxe splice "spread", on remplace ce qu'il y a dans todos par les infos de la DB (findAll), ... = vient d'une fonction
   onMounted(async () => {
-    DB.setApiURL(props.apiURL);
-    // Quand on a déclaré todos c'est une constante, on le splice en tableau, todos.length = là où on commence à ajouter/enlever des éléments (= 0), 0 = on remplace 0 éléments, ... = syntaxe splice "spread", on remplace ce qu'il y a dans todos par les infos de la DB (findAll), ... = vient d'une fonction
-    todos.splice(todos.length, 0, ...(await DB.findAll()));
-    // console.table(todos);
+    todosStore.init(props.apiURL);
   });
-
-  // FONCTIONS CRUD
-  const createItem = async (content) => {
-    const todo = await DB.create(content);
-    todos.push(todo);
-  };
-
-  // deleteOneById(id)
-  // event: on-delete de Todo.vue
-  const deleteOneById = async (id) => {
-    await DB.deleteOneById(id);
-    todos.splice(
-      todos.findIndex((todo) => todo.id === id),
-      1
-    );
-  };
 </script>
 
 <template>
@@ -48,7 +24,7 @@
       <h2 id="todo-heading" class="sr-only">Todo list</h2>
 
       <!-- INPUT PRINCIPAL -->
-      <TodoListAddForm @on-submit-add-form="createItem($event)" />
+      <TodoListAddForm @on-submit-add-form="todosStore.createItem($event)" />
 
       <!-- LISTE DES TODOS -->
       <ul
@@ -58,16 +34,16 @@
       >
         <!-- ITEM (exemple) -->
         <todo
-          v-for="todo in todos"
+          v-for="todo in todosStore.todos"
           :key="todo.id"
           :todo="todo"
-          @on-delete="deleteOneById($event)"
+          @on-delete="todosStore.deleteOneById($event)"
         />
         <!-- ($event) ou rien sans parenthèses -->
       </ul>
 
       <!-- FOOTER DE LISTE -->
-      <TodoListFooter :notCompletedCount="notCompletedCount" />
+      <TodoListFooter :notCompletedCount="todosStore.notCompletedCount" />
     </section>
 </template>
 
